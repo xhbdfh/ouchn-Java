781e3873cc1b2629aba4999c8f9baab3de5b9741
liyue <liyue@bogon>
2018-03-23 15:17:05 +0800

    MacBook commit

---
+++ p16MyApplication/src/p16myapplication/MyClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p16myapplication;
+
+/**
+ *
+ * @author liyue
+ */
+public class MyClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+    }
+    
+}
