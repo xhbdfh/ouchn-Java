4d1175062dacf5ca0f9b52fde36feb0f1e7c58dc
liyue <liyue@SZB01700.bitland.com.cn>
2018-03-23 14:30:15 +0800

    P21

---
+++ p20MyApplication/src/p20myapplication/MyClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p20myapplication;
+
+/**
+ *
+ * @author liyue
+ */
+public class MyClass {
+    private int val1,val2;
+    public void myFun(int x ,int y){
+        val1=x;
+        val2=y;
+    System.out.println("The sum is:" + (val1 + val2));
+    }
+    public static void main(String arg[]){
+        MyClass MyObj=new MyClass();
+        MyObj.myFun(1,2);
+    }
+}
