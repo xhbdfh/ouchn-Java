6c29dd72d407390f0534b22af49747cfd3b6b4e3
liyue <xhbdfh@gmail.com>
2018-03-26 16:39:47 +0800

    p32 数学类Math

--- p32Math/src/p32math/P32Math.java
+++ p32Math/src/p32math/P32Math.java
@@ -21,6 +21,18 @@ public class P32Math {
           System.out.println(Math.tan(0.5));
           System.out.println(Math.round(6.6));
           System.out.println(Math.round(6.3));
+          System.out.println(Math.ceil(9.1));
+          System.out.println(Math.ceil(-9.7));
+          System.out.println(Math.floor(9.1));
+          System.out.println(Math.floor(-9.7));
+          System.out.println(Math.sqrt(144));
+          System.out.println(Math.pow(5,2));
+          System.out.println(Math.exp(2));
+          System.out.println(Math.log(7.38905609893065));
+          System.out.println(Math.max(570,198));
+          System.out.println(Math.min(570,198));
+          System.out.println(Math.random());         
+          
     }
     
 }
