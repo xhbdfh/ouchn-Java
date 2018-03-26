2fab57c326c8800b31757b765dbceaee96f6c701
liyue <liyue@SZB01700.bitland.com.cn>
2018-03-26 14:59:36 +0800

    P21

---
+++ p40/src/p40/P40.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p40;
+
+/**
+ *
+ * @author liyue
+ */
+public class P40 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i = 1 ; i<=9; i++){
+            for(int j=1;j<=i;j++)
+            System.out.print(" "+ i + "*" + j + "=" + i * j)；
+            System.out.println();
+                            
+        }
+    }
+    
+}
