f0d16bfe6ba615496c563e945d921de458bdee11
liyue <liyue@SZB01700.bitland.com.cn>
2018-03-26 15:01:39 +0800

    P21

--- p40/src/p40/P40.java
+++ p40/src/p40/P40.java
@@ -18,7 +18,7 @@ public class P40 {
         // TODO code application logic here
         for(int i = 1 ; i<=9; i++){
             for(int j=1;j<=i;j++)
-            System.out.print(" "+ i + "*" + j + "=" + i * j)；
+            System.out.print("  " + i + "*" + j + "="+i*j)；
             System.out.println();
                             
         }
