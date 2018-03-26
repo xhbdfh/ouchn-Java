1accb853c2ba1d3cf218855928efed9249b4ec6c
liyue <liyue@SZB01700.bitland.com.cn>
2018-03-26 15:04:50 +0800

    P40

--- p40/src/p40/P40.java
+++ p40/src/p40/P40.java
@@ -19,8 +19,7 @@ public class P40 {
         for(int i = 1 ; i<=9; i++){
             for(int j=1;j<=i;j++)
             System.out.print("  " + i + "*" + j + "="+i*j)；
-            System.out.println();
-                            
+            System.out.println();                            
         }
     }
     
