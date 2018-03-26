ed0683e3fcad78af784f56c9523ab81c2d1c587b
liyue <liyue@SZB01700.bitland.com.cn>
2018-03-26 15:25:48 +0800

    p42 Break语句 九九乘法表

---
+++ p42/src/p42/MultiTable.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p42;
+
+/**
+ *
+ * @author liyue
+ */
+public class MultiTable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i=1;i<=9;i++){
+            for(int j=1;j<=9;j++){
+                if(j>i)
+                    break;
+                System.out.print("   "+i+" * "+j+" =" +i*j);
+                }
+            System.out.println();
+            }
+        }
+    }
+    
+}
