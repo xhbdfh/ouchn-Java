3bbba46b741cd58034e4de2427314143bf7ae3f7
liyue <liyue@SZB01700.bitland.com.cn>
2018-03-26 16:31:43 +0800

    p32 数学类Math

---
+++ p32Math/src/p32math/P32Math.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p32math;
+
+/**
+ *
+ * @author liyue
+ */
+public class P32Math {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          System.out.println(Math.sin(0));
+          System.out.println(Math.cos(0));
+          System.out.println(Math.tan(0.5));
+          System.out.println(Math.round(6.6));
+          System.out.println(Math.round(6.3));
+    }
+    
+}
