0360c91ed92c1892669806f02a9d921d24afdc23
liyue <liyue@SZB01700.bitland.com.cn>
2018-03-26 14:59:29 +0800

    P21

---
+++ p27/src/p27/P27.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p27;
+
+/**
+ *
+ * @author liyue
+ */
+public class P27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        final int PRICE =30;
+        System.out.println(PRICE);
+        final double PI=3.141592654;
+        System.out.println(PI);
+    }
+    
+}
